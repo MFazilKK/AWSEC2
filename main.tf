9fc1976610876407efa2f68d9b93d21a36859178
Mohammed Fazil KK <109734112+MFazilKK@users.noreply.github.com>
2023-08-16 17:42:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-08a52ddb321b32a8c"  # Replace with a valid AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.my_subnet.id
-  associate_public_ip_address = true  # Prevent public IP assignment
+  
 
   # Other instance configuration, like key_name, security_groups, etc.
 }
